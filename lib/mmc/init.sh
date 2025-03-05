957ee95d04af84e8cc41fc84d7ee1e1a8f55017f
kinglove1990 <295856977@qq.com>
2025-03-05 20:59:07 +0800

    Update init.sh

--- lib/mmc/init.sh
+++ lib/mmc/init.sh
@@ -1,5 +1,13 @@
 #!/bin/ash
-data_mmcblk=$(grep -rn PARTNAME /sys/block/mmcblk0 | grep -w data | cut -d '/' -f 5)
+
+# 查找包含 PARTNAME=data 的 mmcblk0 分区
+data_mmcblk=$(grep -rn 'PARTNAME=data' /sys/block/mmcblk0 | cut -d: -f1 | cut -d/ -f3)
+
+# 创建挂载点
 mkdir -p /mnt/data
-cryptsetup --key-file=/lib/mmc/data_key luksOpen /dev/$data_mmcblk data
+
+# 解密分区并映射为 /dev/mapper/data
+cryptsetup --key-file=/lib/mmc/data_key luksOpen /dev/${data_mmcblk} data
+
+# 挂载解密后的分区到 /mnt/data
 mount /dev/mapper/data /mnt/data
